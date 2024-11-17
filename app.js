253b96f07292d80ce9fa4c55ad9b915923c3b078
Frozanziaee <130326629+Frozanziaee@users.noreply.github.com>
2024-11-17 20:06:53 +0430

    update json file

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 require('express-async-errors')
 
 // Extra security packages
-// const helmet = require('helmet')
+const helmet = require('helmet')
 const cors = require('cors')
-// const rateLimiter = require('express-rate-limit')
-// const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+const xss = require('xss-clean')
 
 const express = require('express')
 const app = express()
@@ -18,28 +18,36 @@ const authenticateUser = require('./middleware/authentication')
 const authRouter = require('./routes/auth')
 const tasksRouter = require('./routes/tasks')
 const projectsRouter = require('./routes/projects')
+const usersRouter = require('./routes/users')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-// app.use(
-//   rateLimiter({
-//     windowMS: 15 * 60 * 1000, // 15 minutes
-//     max: 100 // limit each IP to 100 requests per windowMs
-//   })
-// )
+app.use(
+  rateLimiter({
+    windowMS: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json())
-// app.use(helmet())
-app.use(cors())
-// app.use(xss())
-// extra packages
+app.use(helmet())
+app.use(xss())
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
+}))
 
-app.get('/', (req, res) => {
-  res.send('Welcome Tasksaster')
+
+app.use((req, res, next) => {
+  console.log("REQUEST BODY", req.body)
+  req.port = port
+  next()
 })
+
 // routes
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/tasks', authenticateUser, tasksRouter)
 app.use('/api/v1/projects', authenticateUser, projectsRouter)
 
